9bed3444b3baa46e245942e49117a87454859f94
Matteo Tambuscio <matteotambuscio35@gmail.com>
2023-03-02 18:25:03 +0100

    add bubble sort to es1.py

--- homework 1/es1.py
+++ homework 1/es1.py
@@ -1,9 +1,30 @@
 
+def bubbleSort(lista):
 
+    l = len(lista)
+    
+    stop = True
+
+    for i in range(0, l-1):
+        if (lista[i+1] < lista[i]):
+            
+            lista[i+1], lista[i] = lista[i], lista[i+1] 
+
+            stop = False
+    
+    if not stop:
+        return bubbleSort(lista)
+    else:
+        return lista
+    
+         
+
+        
 if __name__ == '__main__':
 
     lista = input("inserisci una lista di numeri separati da virgola: ")
     
     lista = [ int(element) for element in lista.split(',') ]
-
+   
+    lista = bubbleSort(lista)
     print(lista)
