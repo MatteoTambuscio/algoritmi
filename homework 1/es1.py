4165a33e84e973a481e713535931f3ed0759e2a7
Matteo Tambuscio <matteotambuscio35@gmail.com>
2023-03-02 15:33:45 +0100

    prove

--- homework 1/es1.py
+++ homework 1/es1.py
@@ -1,2 +1,9 @@
 
-print("hello matteo tambuscio")
+
+if __name__ == '__main__':
+
+    lista = input("inserisci una lista di numeri separati da virgola: ")
+    
+    lista = [ int(element) for element in lista.split(',') ]
+
+    print(lista)
